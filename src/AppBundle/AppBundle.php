cd1185259b3b599a462e33cd865cd2c84682098f
Benjamin Doherty <bjd@bangpound.org>
2016-03-20 01:33:45 -0500

    remove boilerplate from bundle class.

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -2,24 +2,8 @@
 
 namespace AppBundle;
 
-use AppBundle\DependencyInjection\AppExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\HttpKernel\KernelInterface;
 
 class AppBundle extends Bundle
 {
-    /**
-     * @var KernelInterface
-     */
-    private $kernel;
-
-    public function __construct(KernelInterface $kernel)
-    {
-        $this->kernel = $kernel;
-    }
-
-    public function getContainerExtension()
-    {
-        return new AppExtension();
-    }
 }

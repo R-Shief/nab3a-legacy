434e84432fe71b41cb2013bad129b65f69055353
Benjamin Doherty <bjd@bangpound.org>
2016-03-21 03:32:21 -0500

    remove junk from application class.

--- src/Nab3aBundle/Console/Application.php
+++ src/Nab3aBundle/Console/Application.php
@@ -4,9 +4,6 @@ namespace Nab3aBundle\Console;
 
 use Symfony\Component\Console\Application as BaseApplication;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\HttpKernel\KernelInterface;
 
 class Application extends BaseApplication
 {
@@ -18,22 +15,4 @@ class Application extends BaseApplication
     const MINOR_VERSION = 0;
     const RELEASE_VERSION = 0;
     const EXTRA_VERSION = '';
-
-    public function __construct(ContainerInterface $container, $name = 'UNKNOWN', $version = 'UNKNOWN')
-    {
-        $this->container = $container;
-        parent::__construct($name, $version);
-        $this->getDefinition()->addOption(new InputOption('--env', '-e', InputOption::VALUE_REQUIRED, 'The Environment name.', $this->container->getParameter('kernel.environment')));
-        $this->getDefinition()->addOption(new InputOption('--no-debug', null, InputOption::VALUE_NONE, 'Switches off debug mode.'));
-    }
-
-    /**
-     * Gets the Kernel associated with this Console.
-     *
-     * @return KernelInterface A KernelInterface instance
-     */
-    public function getKernel()
-    {
-        return $this->container->get('kernel');
-    }
 }

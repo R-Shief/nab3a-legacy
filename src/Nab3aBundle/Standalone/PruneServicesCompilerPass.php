c5d1f2c4d0372ca3471dbed056d9010b758a735f
Benjamin Doherty <bjd@bangpound.org>
2016-03-23 00:19:43 -0500

    more pruning container.

--- src/Nab3aBundle/Standalone/PruneServicesCompilerPass.php
+++ src/Nab3aBundle/Standalone/PruneServicesCompilerPass.php
@@ -10,6 +10,8 @@ class PruneServicesCompilerPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
     {
+        // Classes that implement these interfaces are numerous and have
+        // to be brute force removed from the container.
         $removeClasses = [
           'Symfony\\Component\\HttpKernel\\Fragment\\FragmentRendererInterface',
           'Symfony\\Component\\Translation\\Loader\\LoaderInterface',
@@ -18,28 +20,27 @@ class PruneServicesCompilerPass implements CompilerPassInterface
           'Monolog\\Formatter\\FormatterInterface',
         ];
 
-        $definitions = array_filter($container->getDefinitions(), function (Definition $definition) use ($removeClasses) {
-            return empty(array_filter(array_map(function ($removeClass) use ($definition) {
-                return is_a($definition->getClass(), $removeClass, true);
-            }, $removeClasses)));
-        });
-
-        unset($definitions['cache_warmer']);
-        unset($definitions['fragment.handler']);
-        unset($definitions['http_kernel']);
-        unset($definitions['uri_signer']);
-
-        unset($definitions['translation.loader']);
-        unset($definitions['translation.writer']);
-        unset($definitions['translator_listener']);
-        unset($definitions['translator.default']);
-        unset($definitions['monolog.logger.translation']);
-        unset($definitions['request_stack']);
-        unset($definitions['streamed_response_listener']);
-
-        unset($definitions['locale_listener']);
-        unset($definitions['response_listener']);
+        $callback = function (Definition $definition) use ($removeClasses) {
+            return is_a($definition->getClass(), $removeClass, true);
+        };
+        $definitions = array_filter($container->getDefinitions(), $callback);
 
         $container->setDefinitions($definitions);
+
+        $container->removeDefinition('cache_clearer');
+        $container->removeDefinition('cache_warmer');
+        $container->removeDefinition('fragment.handler');
+        $container->removeDefinition('http_kernel');
+        $container->removeDefinition('kernel.class_cache.cache_warmer');
+        $container->removeDefinition('locale_listener');
+        $container->removeDefinition('monolog.logger.translation');
+        $container->removeDefinition('request_stack');
+        $container->removeDefinition('response_listener');
+        $container->removeDefinition('streamed_response_listener');
+        $container->removeDefinition('translation.loader');
+        $container->removeDefinition('translation.writer');
+        $container->removeDefinition('translator_listener');
+        $container->removeDefinition('translator.default');
+        $container->removeDefinition('uri_signer');
     }
 }

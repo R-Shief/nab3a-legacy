ef46690bc2d1be5cc3b546beed39de39f3fb1644
Benjamin Doherty <bjd@bangpound.org>
2016-03-24 05:32:56 -0500

    code style.

--- src/Nab3aBundle/Command/ValidationCommand.php
+++ src/Nab3aBundle/Command/ValidationCommand.php
@@ -3,7 +3,6 @@
 namespace Nab3aBundle\Command;
 
 use Nab3aBundle\Model\StreamParameters;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;

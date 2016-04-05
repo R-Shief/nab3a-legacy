32fcc8c19cf757f7d9e325f35ea1a9a7f1443246
Benjamin Doherty <bjd@bangpound.org>
2016-04-05 01:52:02 -0500

    documentation.

--- src/Nab3aBundle/Stream/TypeGuesser.php
+++ src/Nab3aBundle/Stream/TypeGuesser.php
@@ -11,11 +11,17 @@ class TypeGuesser
      */
     public function getEventName($message)
     {
+        // Blank lines are a keep-alive signal.
         if ($message === "\r\n") {
             $event = 'keep-alive';
         } else {
             $data = \GuzzleHttp\json_decode($message, true);
-            $event = count($data) > 1 ? 'tweet' : key($data);
+            // Twitter public stream messages that decode as objects with only
+            // one property are events of that type. Events may also be objects
+            // with multiple properties including an `event` property. Other
+            // message objects many properties are a standard Tweet payload.
+            // @see <https://dev.twitter.com/streaming/overview/messages-types>
+            $event = count($data) > 1 ? (isset($data['event']) ? $data['event'] : 'tweet') : key($data);
         }
 
         return $event;

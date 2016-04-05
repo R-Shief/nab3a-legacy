03125f773d1af5bffdc6e9203e88b34bfbbc1921
Benjamin Doherty <bjd@bangpound.org>
2016-04-04 23:15:29 -0500

    don't overengineer the guzzle handler definition.

--- src/Nab3aBundle/Guzzle/StackMiddlewareCompilerPass.php
+++ src/Nab3aBundle/Guzzle/StackMiddlewareCompilerPass.php
@@ -2,15 +2,11 @@
 
 namespace Nab3aBundle\Guzzle;
 
-use GuzzleHttp\Handler\CurlHandler;
-use GuzzleHttp\Handler\CurlMultiHandler;
-use GuzzleHttp\Handler\Proxy;
-use GuzzleHttp\Handler\StreamHandler;
+use GuzzleHttp\HandlerStack;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\DefinitionDecorator;
-use Symfony\Component\DependencyInjection\Exception\RuntimeException;
 use Symfony\Component\DependencyInjection\Reference;
 
 class StackMiddlewareCompilerPass implements CompilerPassInterface
@@ -74,39 +70,8 @@ class StackMiddlewareCompilerPass implements CompilerPassInterface
      */
     private static function newHandlerDefinition(ContainerBuilder $container)
     {
-        $definition = null;
-        if (function_exists('curl_multi_exec') && function_exists('curl_exec')) {
-            if (!$container->hasDefinition('nab3a.guzzle.handler.curl_multi')) {
-                $container->register('nab3a.guzzle.handler.curl_multi', CurlMultiHandler::class)->setPublic(false);
-            }
-            if (!$container->hasDefinition('nab3a.guzzle.handler.curl')) {
-                $container->register('nab3a.guzzle.handler.curl', CurlHandler::class)->setPublic(false);
-            }
-            $definition = new Definition(\Closure::class, [new Reference('nab3a.guzzle.handler.curl_multi'), new Reference('nab3a.guzzle.handler.curl')]);
-            $definition->setFactory([Proxy::class, 'wrapSync']);
-        } elseif (function_exists('curl_exec')) {
-            $definition = $container->hasDefinition('nab3a.guzzle.handler.curl')
-              ? $container->getDefinition('nab3a.guzzle.handler.curl')
-              : $container->register('nab3a.guzzle.handler.curl', CurlHandler::class)->setPublic(false);
-        } elseif (function_exists('curl_multi_exec')) {
-            $definition = $container->hasDefinition('nab3a.guzzle.handler.curl_multi')
-              ? $container->getDefinition('nab3a.guzzle.handler.curl_multi')
-              : $container->register('nab3a.guzzle.handler.curl_multi', CurlMultiHandler::class)->setPublic(false);
-        }
-
-        if (ini_get('allow_url_fopen')) {
-            if (!$container->hasDefinition('nab3a.guzzle.handler.stream')) {
-                $container->register('nab3a.guzzle.handler.stream', StreamHandler::class)->setPublic(false);
-            }
-            $definition = $definition
-              ? new Definition(\Closure::class, [$definition, new Reference('nab3a.guzzle.handler.stream')])
-              : $container->getDefinition('nab3a.guzzle.handler.stream');
-            $definition->setFactory([Proxy::class, 'wrapStreaming']);
-        } elseif (!$definition) {
-            // @todo find the right place to say this.
-            throw new RuntimeException('GuzzleHttp requires cURL, the '
-              .'allow_url_fopen ini setting, or a custom HTTP handler.');
-        }
+        $definition = new Definition();
+        $definition->setFactory('GuzzleHttp\choose_handler');
 
         return $definition;
     }

55cc9a6f18b995c94e933fd3eaaf43cade523f73
Benjamin Doherty <bjd@bangpound.org>
2016-04-16 00:24:52 -0500

    remove unused expression function.

--- src/Nab3aBundle/ExpressionLanguage/ExpressionLanguageProvider.php
+++ src/Nab3aBundle/ExpressionLanguage/ExpressionLanguageProvider.php
@@ -13,12 +13,6 @@ class ExpressionLanguageProvider implements ExpressionFunctionProviderInterface
     public function getFunctions()
     {
         return array(
-          new ExpressionFunction('file_get_contents', function ($arg) {
-              return sprintf('file_get_contents(%s)', $arg);
-          }, function (array $variables, $value) {
-              return file_get_contents($value);
-          }),
-
           new ExpressionFunction('nab3a_parameter', function ($arg) {
               return sprintf('$this->get(\'%s\')->get(%s)', 'nab3a.standalone.parameters', $arg);
           }, function (array $variables, $value) {
